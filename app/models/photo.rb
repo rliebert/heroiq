502238af93855f414385954e1f49adc691fdd5aa
Robert Liebert <robert.liebert@gmail.com>
2015-08-13 13:05:10 -0700

    Cleaned up unneeded images

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,25 +3,15 @@ class Photo < ActiveRecord::Base
 	belongs_to :listing
 	belongs_to :product
 
-	if Rails.env.development?
-		has_attached_file :image,
+	has_attached_file :image,
 		:styles => {
 			:thumb => "100x100#",
 			:medium => "400x400>",
 			:large => "800x800>"
 			},
-		:default_url => "no-image_:style.png"
-	else
-		has_attached_file :image,
-			:styles => {
-				:thumb => "100x100#",
-				:medium => "400x400>",
-				:large => "800x800>"
-				},
-			:default_url => "no-image_:style.png",
-			:storage => :dropbox,
-	    	:dropbox_credentials => Rails.root.join("config/dropbox.yml")
-	end
+		:default_url => "no-image_:style.png",
+		:storage => :dropbox,
+    	:dropbox_credentials => Rails.root.join("config/dropbox.yml")
 
   	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
